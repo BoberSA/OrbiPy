7569fe1b0548a15ddfa9ef39724bfa631eaf2c81
Denis Zagorodnev <denis.zagorodnev@gmail.com>
2019-04-01 14:28:13 +0300

    Added some tests

--- orbipyd/test.py
+++ orbipyd/test.py
@@ -20,6 +20,7 @@ leftp = Model.mu1 + 500000 / Model.ER
 rightp = Model.L2 + 500000 / Model.ER
 ev1 = orb.events.event_X(leftp)
 ev2 = orb.events.event_X(rightp)
+events = {'left':[ev1], 'right':[ev2]}
 Corrector = orb.correction.correction_tool()
 Keeper = orb.station_keeping.station_keeping(Model, Corrector, initial_vector)
 
@@ -35,7 +36,9 @@ class integrator_test(unittest.TestCase):
         y = np.array([1.00871674e+00,  9.75960362e-04,  1.29750112e-03,  3.40512141e-04,\
                       7.33582905e-03, -2.06372494e-04])
         result = Integrator.integrate_ode(Model, x, [0, 2 * np.pi])
-        self.assertEqual(np.array(result[-1][:6]).any(), y.any())
+        result = np.array(result[-1][:6]) - y
+        print(result)
+        self.assertTrue(np.linalg.norm(result)<0.1)
         
     def test_brent(self):
         x = np.array([Model.L2 + X0km/Model.ER, 0, Z0km/Model.ER, 0, 0, 0])
@@ -43,8 +46,14 @@ class integrator_test(unittest.TestCase):
         y = np.array([ 1.00869484e+00, -3.60291918e-13,  1.33689840e-03, -1.32608140e-13,\
                       7.62016747e-03,  3.39982679e-13])
         t, result = Integrator.brent(Model, ev1, 0, 0.001, x)
-        print(result, y)
-        self.assertEqual(np.array(result).any(), y.any())
+        result = result - y
+        self.assertTrue(np.linalg.norm(result)<0.1)
+        
+    def test_corrector(self):
+        with self.assertRaises(TypeError):
+            Corrector.corrector(initial_vector, 90, events, 0.05, retit=False, maxit=100)
+    
+        
         
 if __name__ == '__main__':
     unittest.main()   
